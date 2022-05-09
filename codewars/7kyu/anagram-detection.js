9950dd69b714c84adf201a86bf1c0245b02716db
whoisaymen <aymen@qq.com>
2022-05-10 01:09:00 +0800

    Pushed solution

---
+++ codewars/7kyu/anagram-detection.js
@@ -0,0 +1,7 @@
+// Solution: Anagram detection
+var isAnagram = function (test, original) {
+	return (
+		test.toLowerCase().split("").sort().join() ==
+		original.toLowerCase().split("").sort().join()
+	);
+};
