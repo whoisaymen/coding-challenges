9910d5d8594c48e5a6df0df9a97f943d038f3c7b
whoisaymen <aymen@qq.com>
2022-05-14 23:53:13 +0800

    Pushed solution

---
+++ codewars/8yu/es6-string-addition.js
@@ -0,0 +1,5 @@
+// Solution: ES6 string addition
+// https://www.codewars.com/kata/582e4c3406e37fcc770001ad/solutions/javascript
+function joinStrings(string1, string2) {
+	return `${String(string1)} ${String(string2)}`;
+}
