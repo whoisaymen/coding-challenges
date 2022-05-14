ce1d831765afef29d88b2d887ac61a2228ac358e
whoisaymen <aymen@qq.com>
2022-05-14 21:17:48 +0800

    Pushed solution

---
+++ codewars/7kyu/convert-an-array-of-strings-to-array-of-numbers.js
@@ -0,0 +1,5 @@
+// Solution: Convert an array of strings to array of numbers
+// https://www.codewars.com/kata/5783d8f3202c0e486c001d23/
+function toNumberArray(stringarray) {
+	return stringarray.map(Number);
+}
