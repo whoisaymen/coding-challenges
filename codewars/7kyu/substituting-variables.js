9bd692fda0b9fea1eef59435162f649edf3e8db1
whoisaymen <aymen@qq.com>
2022-06-07 16:34:03 +0800

    Pushed solution

---
+++ codewars/7kyu/substituting-variables.js
@@ -0,0 +1,5 @@
+// Solution: Substituting Variables Into Strings: Padded Numbers
+// https://www.codewars.com/kata/51c89385ee245d7ddf000001/
+function solution(value) {
+	return "Value is " + String(value).padStart(5, "0");
+}
