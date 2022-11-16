19a3e39dcfbbada103570bd85358c0f90af322cc
whoisaymen <aymen@qq.com>
2022-11-16 17:29:33 +0100

    pushed solution

---
+++ codewars/7kyu/shorter-concat.js
@@ -0,0 +1,5 @@
+// shorter concat [reverse longer]
+// https://www.codewars.com/kata/54557d61126a00423b000a45/javascript
+function shorter_reverse_longer(a, b) {
+	return a.length < b.length ? `${a}${b.split('').reverse().join('')}${a}` : `${b}${a.split('').reverse().join('')}${b}`;
+}
